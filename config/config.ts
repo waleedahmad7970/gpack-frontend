d8b69c783126e62c9c9a6547e3e830b8d025e115
waleedahmad7970 <waleedahmad7970@gmail.com>
2025-07-16 18:08:07 +0500

    Images issues fixed

--- config/config.ts
+++ config/config.ts
@@ -1,4 +1,4 @@
 // config.ts
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8000/';
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
